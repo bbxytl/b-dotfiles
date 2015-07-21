b3586d4db30829a3c58536ac4fa803b7ee9d4227
bbxytl <tl0485@175game.com>
2015-07-21 11:54:15 +0800

    v1.1-添加中文 man 支持

---
+++ b-man-zh/install-man-zh.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# ====================================================
+#   Copyright (C)2015 All rights reserved.
+#
+#   Author  : bbxytl
+#   Email   : bbxytl@gmail.com
+#   FileName: install.sh
+#   LastModify : 2015-07-20 10:02
+#   Describe:
+#
+#   Log     :
+#
+# ====================================================
+
+if [ $# -ge 2 ];then
+	PASSWD=$1
+	PACKGES=$2
+else
+	echo "请输入密码："
+	read PASSWD
+	PACKGES=$HOME/mydotfiles/packges
+fi
+
+if [ -e $PACKGES ];then mkdir $PACKGES;fi
+
+today=`date +%Y%m%d`
+tmp="$HOME/mydotfiles/tmp"
+if [ ! -e $tmp ];then mkdir $tmp; fi
+
+# 安装 man-zh
+git clone https://github.com/lidaobing/manpages-zh.git $tmp/manpages-zh.$today
+$tmp/manpages-zh.$today
+./configure --prefix=/usr/local/zhman --disable-zhtw 
+make 
+echo $PASSWD | sudo -S make install
+
+# 不使用 b-shell 的话要使用下面命令（在命令行中）
+# cd ~
+# vi .bashrc
+# 在 .bashrc 中增加：
+#	alias cman='man -M /usr/local/zhman/share/man/zh_CN'
+# source .bashrc   # 刷新
+# cman cd          # 查看是否成功，如果要使用英文版，可继续使用 man 命令 
+cd $CURRENT_DIR
+
+
--- install.sh
+++ install.sh
@@ -21,49 +21,6 @@ CURRENT_DIR=`pwd`
 echo " 安装环境，过程中需要输入密码。。。。。请输入："
 read PASSWD
 
-echo $PASSWD | sudo -S yum install -y python-devel.x86_64
-sudo yum groupinstall 'Development Tools'
-sudo rpm -Uvh http://download.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
-echo $PASSWD | sudo -S yum install -y  the_silver_searcher
-
-
-echo " wget install......"
-echo $PASSWD | sudo -S yum install -y  wget
-echo " pip install......."
-rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
-echo $PASSWD | sudo -S yum install -y  epel-release
-echo $PASSWD | sudo -S yum install -y  python-pip
-echo " tmux env install........"
-echo $PASSWD | sudo -S yum install -y  ncurses-devel
-echo $PASSWD | sudo -S yum install -y  autoconf m4 perl automake
-sudo autoconf -ivf
-
-echo $PASSWD | sudo -S  pip install pyflakes
-echo $PASSWD | sudo -S  pip install pylint
-echo $PASSWD | sudo -S  pip install pep8
-pep8str="[pep8]\nmax-line-length = 120"
-if [ ! -e $HOME/.config ]; then mkdir $HOME/.config; fi
-if [ ! -e $HOME/.config/pep8 ];then
-	echo -e pep8str > $HOME/.config/pep8
-fi
-
-# 安装 locate 查找包
-echo $PASSWD | sudo -S  yum install mlocate.x86_64
-echo $PASSWD | sudo -S  updatedb
-
-# 安装 CMake 命令
-install=true
-if locate *cmake*3*2*>/dev/null 2>&1 ;then install=false;fi
-if $install;then
-    if [ ! -e $HOME/tmp ];then mkdir $HOME/tmp ; fi
-    cd $HOME/tmp
-    wget http://www.cmake.org/files/v3.2/cmake-3.2.3.tar.gz
-    tar -zxvf cmake-3.2.3.tar.gz
-    cd cmake-3.2.3
-    ./bootstrap
-    make && echo $PASSWD | sudo -S make install
-    cd $CURRENT_DIR
-fi
 
 today=`date +%Y%m%d`
 cd $HOME
@@ -88,6 +45,12 @@ cd $CURRENT_DIR/b-tmux
 echo $PASSWD | sudo -S chmod +x $CURRENT_DIR/b-powerline/install-powerline.sh
 cd $CURRENT_DIR/b-powerline
 ./install-powerline.sh $PASSWD  $PACKGES
+
+# 配置 中文 man
+cd $CURRENT_DIR/b-man-zh
+echo $PASSWD | sudo -S chmod +x $CURRENT_DIR/b-man-zh/install-man-zh.sh
+./install-man-zh.sh $PASSWD  $PACKGES
+
 
 # 配置 vim
 cd $CURRENT_DIR/b-vim
