80e64a822364a0e2ebb97831cbc823451dbccc00
bbxytl <bbxytl@gmail.com>
2017-06-14 16:07:10 +0800

    tmux，bash

---
+++ b-shell/bash/bashrc.simple.sh
@@ -0,0 +1,11 @@
+
+alias l="ls -al"
+
+# tmux
+alias tmat="tmux attach -t"
+alias tmnn="tmux new -s"
+alias tmls="tmux ls"
+alias tmrn="tmux rename-window"
+alias tmux='tmux -2'
+
+PS1='\[\e[1;35m\][\[\e[1;33m\]\u@\h \[\e[1;31m\]\w\[\e[1;35m\]]\[\e[1;36m\]\$ \[\e[0m\]'
