66964908375942d984970f92984b7f3bb5b587be
bbxytl <bbxytl@gmail.com>
2017-10-11 16:42:26 +0800

    添加 rvm 配置

--- b-shell/bash/clean-docker.sh
+++ b-shell/bash/clean-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Copyright 2017 Théo Chamley
-# Permission is hereby granted, free of charge, to any person obtaining a copy of 
+# Copyright 2017 bbxytl
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (the "Software"), to deal in the Software
 # without restriction, including without limitation the rights to use, copy, modify, merge,
 # publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
