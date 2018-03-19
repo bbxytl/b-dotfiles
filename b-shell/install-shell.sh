4fa9f25a1ddc03d08db1d1dc835d7b5627f60452
bbxytl <bbxytl@gmail.com>
2018-03-19 19:09:08 +0800

    zsh 增加支持 自动补全，自动提示

--- b-shell/install-shell.sh
+++ b-shell/install-shell.sh
@@ -30,16 +30,18 @@ if [ ! -e $bakdot ];then mkdir $bakdot; fi
 echo " Step 1: backing up current config-----------Shell"
 shellbak="$bakdot/ori-shell.$today"
 if [ ! -e $shellbak ];then mkdir $shellbak; fi
-for i in $HOME/.bashrc $HOME/.bash_profile $HOME/.dir_colors $HOME/.inputrc $HOME/.bash_alias.sh $HOME/.bash_env.sh $HOME/.pystartup.py; do [ -e $i ] && [ ! -L $i ] && mv $i $shellbak/; done
 for i in $HOME/.bashrc $HOME/.bash_profile $HOME/.dir_colors $HOME/.inputrc $HOME/.bash_alias.sh $HOME/.bash_env.sh $HOME/.pystartup.py; do [ -L $i ] && unlink $i ; done
+for i in $HOME/.bashrc $HOME/.bash_profile $HOME/.dir_colors $HOME/.inputrc $HOME/.bash_alias.sh $HOME/.bash_env.sh $HOME/.pystartup.py; do [ -e $i ] && [ ! -L $i ] && mv $i $shellbak/; done
 
 echo " Step 2: setting tu symlinks----------Shell"
+B_DOT="$HOME/.b-dot"
 lnif $CURRENT_DIR/bash/bashrc $HOME/.bashrc
 lnif $CURRENT_DIR/bash/bash_profile $HOME/.bash_profile
+
+lnif $CURRENT_DIR/bash/bash_env.sh $B_DOT/bash_env.sh
+lnif $CURRENT_DIR/bash/bash_alias.sh $B_DOT/bash_alias.sh
+
 lnif $CURRENT_DIR/bash/DIR_COLORS $HOME/.dir_colors
-# lnif $CURRENT_DIR/bash/inputrc $HOME/.inputrc
-lnif $CURRENT_DIR/bash/bash_env.sh $HOME/.bash_env.sh
-lnif $CURRENT_DIR/bash/bash_alias.sh $HOME/.bash_alias.sh
 lnif $CURRENT_DIR/bash/pystartup.py $HOME/.pystartup.py
 
 mkdir -p $HOME/.local/bin
