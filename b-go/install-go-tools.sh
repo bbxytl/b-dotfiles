0630719149917bad4f10e40d5553d38c27d815e3
bbxytl <bbxytl@gmail.com>
2019-04-29 18:47:51 +0800

    增加安装 go 的默认下载数据

--- b-go/install-go-tools.sh
+++ b-go/install-go-tools.sh
@@ -21,3 +21,26 @@ git clone --depth 1 --recursive https://github.com/golang/text
 git clone --depth 1 --recursive https://github.com/golang/image
 git clone --depth 1 --recursive https://github.com/golang/debug
 git clone --depth 1 --recursive https://github.com/golang/crypto
+git clone --depth 1 --recursive https://github.com/golang/sync
+
+# github
+mkdir -p ~/go/src/github.com; cd ~/go/src/github.com
+git clone --depth 1 --recursive https://github.com/kisielk/errcheck ./kisielk/errcheck; go install ./kisielk/errcheck
+git clone --depth 1 --recursive https://github.com/mdempsky/gocode ./mdempsky/gocode; go install ./mdempsky/gocode
+git clone --depth 1 --recursive https://github.com/zmb3/gogetdoc ./zmb3/gogetdoc; go install ./zmb3/gogetdoc
+git clone --depth 1 --recursive https://github.com/go-delve/delve/cmd/dlv ./go-delve/delve/cmd/dlv; go install ./go-delve/delve/cmd/dlv
+git clone --depth 1 --recursive https://github.com/josharian/impl ./josharian/impl; go install ./josharian/impl
+git clone --depth 1 --recursive https://github.com/golangci/golangci-lint/cmd/golangci-lint ./golangci/golangci-lint/cmd/golangci-lint; go install ./golangci/golangci-lint/cmd/golangci-lint
+git clone --depth 1 --recursive https://github.com/jstemmer/gotags ./jstemmer/gotags; go install ./jstemmer/gotags
+git clone --depth 1 --recursive https://github.com/fatih/gomodifytags ./fatih/gomodifytags; go install ./fatih/gomodifytags
+git clone --depth 1 --recursive https://github.com/koron/iferr ./koron/iferr; go install ./koron/iferr
+git clone --depth 1 --recursive https://github.com/klauspost/asmfmt/cmd/asmfmt ./klauspost/asmfmt/cmd/asmfmt; go install ./klauspost/asmfmt/cmd/asmfmt
+git clone --depth 1 --recursive https://github.com/alecthomas/gometalinter ./alecthomas/gometalinter; go install ./alecthomas/gometalinter
+
+# go install
+cd ~/go/src/golang.org/x/lint/golint; go install ./
+cd ~/go/src/golang.org/x/tools/cmd/gorename; go install ./
+cd ~/go/src/golang.org/x/tools/cmd/goimports; go install ./
+cd ~/go/src/golang.org/x/tools/cmd/guru; go install ./
+cd ~/go/src/golang.org/x/tools/cmd/gopls; go install ./
+
